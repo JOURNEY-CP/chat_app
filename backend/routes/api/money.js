1e249a8257098d9d156d585c960bf20cefa9b670
CHILUKURI SRI HARSHA <sharshach@gmail.com>
2020-12-05 19:15:04 +0530

    Add : Api to create a money account

--- backend/routes/api/money.js
+++ backend/routes/api/money.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
+var moneyDb=require('../../db/money');
+let monetDbConnect;
+
 const moneyRouter = DBConnect => {
+  moneyDbConnect=moneyDb(DBConnect);
   return router;
 };
 
 router.get('/', function(req, res, next) {
   res.send("hello");
 });
+router.post('/',(req,res)=>{
+  moneyDbConnect.
+    createNewAccount(req.body.id,req.body.user_id,req.body.pin)
+    .then(()=>res.send("created"))
+    .catch(err=>res.status(500).send(err));
+})
 
 module.exports = moneyRouter;
